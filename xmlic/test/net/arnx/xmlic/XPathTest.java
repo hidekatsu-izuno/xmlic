4b2c6efe85499689a4b112872dae390995bfc8a9
hidekatsu-izuno <hidekatsu.izuno@gmail.com>
2013-08-08 23:10:47 +0900

    0.9.0 リリース

--- xmlic/test/net/arnx/xmlic/XPathTest.java
+++ xmlic/test/net/arnx/xmlic/XPathTest.java
@@ -1,10 +1,6 @@
 package net.arnx.xmlic;
 
-import static org.junit.Assert.*;
-
-import net.arnx.xmlic.internal.org.jaxen.JaxenHandler;
-import net.arnx.xmlic.internal.org.jaxen.saxpath.XPathReader;
-import net.arnx.xmlic.internal.org.jaxen.saxpath.helpers.XPathReaderFactory;
+import static org.junit.Assert.assertEquals;
 import net.arnx.xmlic.internal.util.XMLContext;
 import net.arnx.xmlic.internal.util.XmlicXPath;
 
@@ -17,7 +13,5 @@ public class XPathTest {
 		assertEquals(new XmlicXPath(xcontext, ".//a", false).toString(), new XmlicXPath(xcontext, ".//a", true).toString());
 		assertEquals(new XmlicXPath(xcontext, "descendant-or-self::node()/a[@name]", false).toString(), new XmlicXPath(xcontext, "a[@name]", true).toString());
 		assertEquals(new XmlicXPath(xcontext, "descendant-or-self::node()/child::node()[@name='test']", false).toString(), new XmlicXPath(xcontext, "@name='test'", true).toString());
-		System.out.println(new XmlicXPath(xcontext, "li[position()=2]", true));
-		System.out.println(new XmlicXPath(xcontext, "child::li[position()=2]", true));
 	}
 }
