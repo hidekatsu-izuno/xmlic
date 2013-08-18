47a711c979d877e39bee774009f6587d98c7f736
hidekatsu-izuno <hidekatsu.izuno@gmail.com>
2013-08-18 11:44:45 +0900

    ドキュメント更新

--- xmlic/src/net/arnx/xmlic/Filter.java
+++ xmlic/src/net/arnx/xmlic/Filter.java
@@ -1,5 +1,17 @@
 package net.arnx.xmlic;
 
+/**
+ * This interface uses for filtering value.
+ * 
+ * @param <T> filtering value's type.
+ */
 public interface Filter<T> {
+	/**
+	 * Filters value.
+	 * 
+	 * @param value filtering value
+	 * @param status current callback status
+	 * @return filtered value
+	 */
 	public T filter(T value, Status status);
 }
--- xmlic/src/net/arnx/xmlic/Visitor.java
+++ xmlic/src/net/arnx/xmlic/Visitor.java
@@ -1,5 +1,16 @@
 package net.arnx.xmlic;
 
+/**
+ * This interface uses for the callback iteration.
+ * 
+ * @param <T> iterated value's type
+ */
 public interface Visitor<T> {
+	/**
+	 * Callback if current value visited.
+	 * 
+	 * @param current current value
+	 * @param status current callback status
+	 */
 	public void visit(T current, Status status);
 }
